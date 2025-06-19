adb0f7592f15191b21903b7900d92f957d14d6e3
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-19 09:09:58 +0200

    Data_Types_Python

--- PythonBasics/Demo2.py
+++ PythonBasics/Demo2.py
@@ -4,6 +4,29 @@ values = [1, 2, "rahul", 4, 5]
 
 print(values[0]) #1 
 
+print(values[2]) #rahul
+
+print(values[-1]) #5
+
+print(values[1:3]) # [2, 'rahul'] - Slicing the list
+
+# Adding an element to the list
+values.append(3,"shetty")
+print(values)
+
+values.append("shetty")
+print(values)
+
+# Updated list
+values[0] = 10
+
+print(values)  # [10, 2, 'rahul', 4, 5, 3, 'shetty']
+
+# Removing an element from the list
+values.remove("shetty")
+print(values)  # [10, 2, 'rahul', 4, 5, 3]
+
+
 
 
 
