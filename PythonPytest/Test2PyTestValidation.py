39b737f3aa86aebf80533ccd10e1bd2b0388eb30
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-19 19:51:19 +0200

    Changes

--- PythonPytest/Test2PyTestValidation.py
+++ PythonPytest/Test2PyTestValidation.py
@@ -7,6 +7,12 @@ import pytest
 def test_thirdCheck(preSetupWork):
     print("This is Third Test")
 
+@pytest.fixture(scope="module") #@scope = module
+def preWork():
+    print("I setup browser instance")
+    return "pass"
+
+
 
 
 
