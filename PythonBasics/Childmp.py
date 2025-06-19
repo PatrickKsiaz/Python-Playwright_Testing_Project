ad4fcd0faf2be9ee3d0abbcd5f13a7ee39b624c0
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-19 17:28:03 +0200

    Changes_

--- PythonBasics/Childmp.py
+++ PythonBasics/Childmp.py
@@ -3,9 +3,13 @@ from OopsDemo import Calculator
 class ChildImpl(Calculator):
     num2 = 200
 
+    def __init__(self):
+        Calculator.__init__(self, 2 , 10)
+
+
     def getCompleteData(self):
         return self.num2 + self.num + self.Summation()
 
-    
+obj = ChildImpl()
+print(obj.getCompleteData())  # This will print the sum of num2, num, and the result of Summation()
 
-    
