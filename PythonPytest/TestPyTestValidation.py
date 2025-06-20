f16924acbcc6545aefbe55f8eb2f269ad6ebc5bd
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-20 21:02:59 +0200

    Changes_20/06

--- PythonPytest/TestPyTestValidation.py
+++ PythonPytest/TestPyTestValidation.py
@@ -7,12 +7,18 @@ def preWork():
     print("I setup browser instance")
     return "pass"
 
+@pytest.fixture(scope="module") #@scope = module
+def secondWork():
+    print("I setup module instance")
+    yield
+    print("tear down validation")
 
-def test_initialCheck(preWork):
+
+def test_initialCheck(preWork,secondWork):
     print("This is first test")
     assert preWork == "fail"
 
 
-def test_initialCheck(preWork):
+def test_initialCheck(preWork, secondWork):
     print("This is second test")
 
