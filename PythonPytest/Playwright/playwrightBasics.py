1cabcd9c1c736b7a05c9ac33f7014c6d50f57227
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-21 12:00:32 +0200

    Changes_Playwright

--- PythonPytest/Playwright/playwrightBasics.py
+++ PythonPytest/Playwright/playwrightBasics.py
@@ -1,5 +1,11 @@
-
+from playwright.sync_api import Page
 
 
 def test_playwrightBasics(playwright):
-    playwright.chromium.launch(headless=False).new_page().goto("https://www.google.com")
+    playwright.chromium.launch(headless=False).new_page().goto("https://rahulshettyacademy.com")
+
+
+#chromium engine in Headless mode, 1 single context
+def test_playwrightShortCut(page: Page):
+    page.goto("https://rahulshettyacademy.com")
+    
