0780becfb6c17fa29e22161b19e8845d4c246391
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-23 20:55:53 +0200

    Changes_Firefox_BrowserTest

--- PythonPytest/Playwright/playwrightBasics.py
+++ PythonPytest/Playwright/playwrightBasics.py
@@ -1,9 +1,9 @@
-import time
 from playwright.sync_api import Page
 
 
 def test_playwrightBasics(playwright):
     playwright.chromium.launch(headless=False).new_page().goto("https://rahulshettyacademy.com")
+    playwright.firefox.launch(headless=False).new_page().goto("https://rahulshettyacademy.com")
 
 
 #chromium engine in Headless mode, 1 single context
@@ -15,16 +15,31 @@ def test_playwrightShortCut(page: Page):
 def test_coreLocators(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise")
     page.get_by_label("Username:").fill("rahukshettyacademy")
-    page.get_by_label("Password").fill("learn")
+    page.get_by_label("Password").fill("learning")
     page.get_by_role("combobox").select_option("teach")
     page.locator("#terms").check()
-    page.get_by_role(role:"link", name="terms and conditions").click()
-    page.get_by_role(role:"button", name="Sign In").click()
-   
-    time.sleep(5)  # Wait for 5 seconds to observe the result
+    page.get_by_role(role="link", name="terms and conditions").click()
+    page.get_by_role(role="button", name="Sign In").click()
+    expect(page.get_by_text("Incorrect username/password")).to_be_visible() # Wait for the error message to be visible
 
-    #page.get_by_role("button", name="Sign In").click()
     
+def test_firefoxBrowser(playwright):
+    test_firefoxBrowser = playwright.firefox.launch(headless=False) # Launch Firefox in non-headless mode
+    browser = firefoxBrowser.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://rahulshettyacademy.com/loginpagePractise")
+    page.get_by_label("Username:").fill("rahukshettyacademy")
+    page.get_by_label("Password").fill("learning")
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role(role="link", name="terms and conditions").click()
+    page.get_by_role(role="button", name="Sign In").click()
+    expect(page.get_by_text("Incorrect username/password")).to_be_visible() # Wait for the error message to be visible
+
+
+    
+
+
 
 
     
