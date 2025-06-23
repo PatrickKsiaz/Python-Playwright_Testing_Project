5ee3027d5163674d3b6daf5b500958ff87152e3d
Patryk Ksiazek <globalghost1@protonmail.com>
2025-06-23 20:34:58 +0200

    Changes_Playwright

--- PythonPytest/Playwright/playwrightBasics.py
+++ PythonPytest/Playwright/playwrightBasics.py
@@ -11,13 +11,20 @@ def test_playwrightShortCut(page: Page):
     page.goto("https://rahulshettyacademy.com")
     
 
+# -- checking by ID selector #terms .text-info
 def test_coreLocators(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise")
     page.get_by_label("Username:").fill("rahukshettyacademy")
-    page.get_by_label("Password").fill("learning")
+    page.get_by_label("Password").fill("learn")
     page.get_by_role("combobox").select_option("teach")
-    time.sleep(5)
-    page.locator("#signInBtn").click()
-    assert page.locator(".alert-success").is_visible()
+    page.locator("#terms").check()
+    page.get_by_role(role:"link", name="terms and conditions").click()
+    page.get_by_role(role:"button", name="Sign In").click()
+   
+    time.sleep(5)  # Wait for 5 seconds to observe the result
+
+    #page.get_by_role("button", name="Sign In").click()
     
+
+
     
